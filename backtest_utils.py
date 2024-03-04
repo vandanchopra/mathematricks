06c1d03a38ded6d55ec4327e8f24fa0d86efca0a
avmhdt <josessc.js@gmail.com>
2024-03-04 02:30:39 -0500

    Saving tests to backtests folder, plus load backtest function

--- backtest_utils.py
+++ backtest_utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import pickle
+from os import listdir
 import numpy as np
 from stock_automation_utils import StockAutomationUtils
 from strategy_vault import StrategyVault
@@ -135,8 +136,19 @@ class TradingSimulator:
                 pbar.update(1)
                 
         test = {'strategy_name':self.strategy.get_name(), 
-                'inputs':{'symbols':symbols, 'start_date_dt':start_date_dt, 'end_date_dt':end_date_dt, 'rebalance_frequency':rebalance_frequency, 'long_count':long_count, 'short_count':short_count, 'portfolio_starting_value':portfolio_starting_value, 'risk_pct':risk_pct, 'reinvest_profits_bool':reinvest_profits_bool}, 
+                'inputs':{'symbols':symbols, 'start_date_dt':start_date_dt.date(), 'end_date_dt':end_date_dt.date(), 'rebalance_frequency':rebalance_frequency, 'long_count':long_count, 'short_count':short_count, 'portfolio_starting_value':portfolio_starting_value, 'risk_pct':risk_pct, 'reinvest_profits_bool':reinvest_profits_bool},
                 'rebalance_periods':rebalance_periods, 
                 'backtest_runs': backtest_runs, 
                 'backtest_profits': backtest_profits}
+
+        pickle_filename = f'backtests/{test["strategy_name"]}_{"_".join(test["inputs"]["symbols"])}_{"_".join([str(x) for x in list(test["inputs"].values())[1:]])}.pkl'
+        pickle.dump(test, open(pickle_filename, 'wb'))
+        print(f'Backtest results saved to {pickle_filename}')
+
+        return test
+
+    def load_backtest(self, symbols, start_date_dt, end_date_dt, rebalance_frequency, long_count, short_count, portfolio_starting_value, risk_pct, reinvest_profits_bool):
+        pickle_filename = f'backtests/{self.strategy.get_name()}_{"_".join(symbols)}_{start_date_dt.date()}_{end_date_dt.date()}_{rebalance_frequency}_{long_count}_{short_count}_{portfolio_starting_value}_{risk_pct}_{reinvest_profits_bool}.pkl'
+        test = pickle.load(open(pickle_filename, 'rb'))
+        print(f'Backtest results loaded from {pickle_filename}')
         return test
