e44b1ccb19e5a6b40420d0213498993322d31caf
avmhdt <josessc.js@gmail.com>
2024-03-04 03:16:25 -0500

    analysis_utils.py

--- backtest_utils.py
+++ backtest_utils.py
@@ -146,9 +146,3 @@ class TradingSimulator:
         print(f'Backtest results saved to {pickle_filename}')
 
         return test
-
-    def load_backtest(self, symbols, start_date_dt, end_date_dt, rebalance_frequency, long_count, short_count, portfolio_starting_value, risk_pct, reinvest_profits_bool):
-        pickle_filename = f'backtests/{self.strategy.get_name()}_{"_".join(symbols)}_{start_date_dt.date()}_{end_date_dt.date()}_{rebalance_frequency}_{long_count}_{short_count}_{portfolio_starting_value}_{risk_pct}_{reinvest_profits_bool}.pkl'
-        test = pickle.load(open(pickle_filename, 'rb'))
-        print(f'Backtest results loaded from {pickle_filename}')
-        return test
