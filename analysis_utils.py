5fe25437e2c69e881b3ee028ba3c3bc00fec2445
avmhdt <josessc.js@gmail.com>
2024-03-04 03:18:07 -0500

    corrected typo

--- analysis_utils.py
+++ analysis_utils.py
@@ -1,6 +1,29 @@
+from datetime import datetime, timedelta
+import pickle
+import numpy as np
+from stock_automation_utils import StockAutomationUtils
+from strategy_vault import StrategyVault
+import pandas as pd
+from tqdm import tqdm
 
-def load_backtest(self, symbols, start_date_dt, end_date_dt, rebalance_frequency, long_count, short_count, portfolio_starting_value, risk_pct, reinvest_profits_bool):
-    pickle_filename = f'backtests/{self.strategy.get_name()}_{"_".join(symbols)}_{start_date_dt.date()}_{end_date_dt.date()}_{rebalance_frequency}_{long_count}_{short_count}_{portfolio_starting_value}_{risk_pct}_{reinvest_profits_bool}.pkl'
-    test = pickle.load(open(pickle_filename, 'rb'))
-    print(f'Backtest results loaded from {pickle_filename}')
-    return test
+
+class BacktestAnalyzer:
+    def __init__(self, strategy_name, symbols, start_date_dt, end_date_dt, rebalance_frequency, long_count, short_count,
+                  portfolio_starting_value, risk_pct, reinvest_profits_bool):
+        self.strategy_name = strategy_name
+        self.symbols = symbols
+        self.start_date_dt = start_date_dt
+        self.end_date_dt = end_date_dt
+        self.rebalance_frequency = rebalance_frequency
+        self.long_count = long_count
+        self.short_count = short_count
+        self.portfolio_starting_value = portfolio_starting_value
+        self.risk_pct = risk_pct
+        self.reinvest_profits_bool = reinvest_profits_bool
+
+    def load_backtest(self):
+        pickle_filename = f'backtests/{self.strategy_name}_{"_".join(self.symbols)}_{self.start_date_dt.date()}_{self.end_date_dt.date()}_{self.rebalance_frequency}_{self.long_count}_{self.short_count}_{self.portfolio_starting_value}_{self.risk_pct}_{self.reinvest_profits_bool}.pkl'
+        test = pickle.load(open(pickle_filename, 'rb'))
+        print(f'Backtest results loaded from {pickle_filename}')
+
+        return test
